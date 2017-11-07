a317b2b7f3f4cdde42bcfb356ee009a99ab425eb
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-07 18:16:28 +0100

    Add morphologies

---
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/morphology/morphologies/discourse/Discourse.kt
@@ -0,0 +1,12 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.linguisticdescription.morphology.morphologies.discourse
+
+import com.kotlinnlp.linguisticdescription.morphology.morphologies.Morphology
+
+interface Discourse : Morphology
---
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/morphology/morphologies/relations/atemporal/Adverb.kt
@@ -0,0 +1,81 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.linguisticdescription.morphology.morphologies.relations.atemporal
+
+import com.kotlinnlp.linguisticdescription.morphology.morphologies.ContentWord
+
+/**
+ *
+ */
+sealed class Adverb : Atemporal {
+
+  /**
+   *
+   */
+  class Modal : Adverb(), ContentWord
+
+  /**
+   *
+   */
+  class Comparative : Adverb()
+
+  /**
+   *
+   */
+  class Indefinite : Adverb()
+
+  /**
+   *
+   */
+  class Interrogative : Adverb()
+
+  /**
+   *
+   */
+  class Limiting : Adverb()
+
+  /**
+   *
+   */
+  class Locative : Adverb()
+
+  /**
+   *
+   */
+  class Negative : Adverb()
+
+  /**
+   *
+   */
+  class Phrase : Adverb()
+
+  /**
+   *
+   */
+  class Quantitative : Adverb()
+
+  /**
+   *
+   */
+  class Strength : Adverb()
+
+  /**
+   *
+   */
+  class Time : Adverb()
+
+  /**
+   *
+   */
+  class Concessive : Adverb()
+
+  /**
+   *
+   */
+  class Deictic : Adverb()
+}
