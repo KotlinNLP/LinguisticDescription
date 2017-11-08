2c695810a7952c7f23a107f595d5f58205e13623
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-08 12:18:46 +0100

    Update documentation

--- src/main/kotlin/com/kotlinnlp/linguisticdescription/morphology/morphologies/relations/atemporal/Adverb.kt
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/morphology/morphologies/relations/atemporal/Adverb.kt
@@ -15,67 +15,67 @@ import com.kotlinnlp.linguisticdescription.morphology.morphologies.ContentWord
 sealed class Adverb : Atemporal {
 
   /**
-   *
+   * The 'modal adverb' morphology.
    */
   class Modal : Adverb(), ContentWord
 
   /**
-   *
+   * The 'comparative adverb' morphology.
    */
   class Comparative : Adverb()
 
   /**
-   *
+   * The 'indefinite adverb' morphology.
    */
   class Indefinite : Adverb()
 
   /**
-   *
+   * The 'interrogative adverb' morphology.
    */
   class Interrogative : Adverb()
 
   /**
-   *
+   * The 'limiting adverb' morphology.
    */
   class Limiting : Adverb()
 
   /**
-   *
+   * The 'locative adverb' morphology.
    */
   class Locative : Adverb()
 
   /**
-   *
+   * The 'negative adverb' morphology.
    */
   class Negative : Adverb()
 
   /**
-   *
+   * The 'adverbial phrase' morphology.
    */
   class Phrase : Adverb()
 
   /**
-   *
+   * The 'quantitative adverb' morphology.
    */
   class Quantitative : Adverb()
 
   /**
-   *
+   * The 'strength adverb' morphology.
    */
   class Strength : Adverb()
 
   /**
-   *
+   * The 'time adverb' morphology.
    */
   class Time : Adverb()
 
   /**
-   *
+   * The 'concessive adverb' morphology.
    */
   class Concessive : Adverb()
 
   /**
-   *
+   * The 'deictic adverb' morphology.
    */
   class Deictic : Adverb()
 }
