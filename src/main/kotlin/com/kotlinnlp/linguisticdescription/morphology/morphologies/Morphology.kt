6592117e311fcaa09efd6ca624ba0b0ec684fe5b
KotlinNLP Authors <github@kotlinnlp.com>
2017-11-15 12:32:33 +0100

    Convert to abstract class, implement toString() and getProperties() methods

--- src/main/kotlin/com/kotlinnlp/linguisticdescription/morphology/morphologies/Morphology.kt
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/morphology/morphologies/Morphology.kt
@@ -8,14 +8,54 @@
 package com.kotlinnlp.linguisticdescription.morphology.morphologies
 
 import com.kotlinnlp.linguisticdescription.morphology.MorphologyType
+import com.kotlinnlp.linguisticdescription.morphology.properties.MorphologyProperty
+import kotlin.reflect.KClass
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
 
 /**
  * The base interface implemented by all morphologies.
  */
-interface Morphology {
+abstract class Morphology {
 
   /**
    * The type associated to this morphology.
    */
-  val type: MorphologyType
+  abstract val type: MorphologyType
+
+  /**
+   * @return a map of properties names to values
+   */
+  fun getProperties(): Map<String, MorphologyProperty> {
+
+    val paramsNames: Set<String> = this::class.primaryConstructor!!.parameters.map { it.name!! }.toSet()
+
+    return this::class.memberProperties
+      .filter { it.name in paramsNames }
+      .associate { it.name to it.getter.call(this) as MorphologyProperty }
+  }
+
+  /**
+   * @return the string representation of this morphology (with the values of its properties)
+   */
+  override fun toString() = "%s (%s)".format(
+    this.getSuperClassesNames().joinToString(separator = "."),
+    this.getProperties().entries.joinToString { "${it.key}: ${it.value}" }
+  )
+
+  /**
+   * @return the list of names of super classes of this morphology, from the highest to itself
+   */
+  private fun getSuperClassesNames(): List<String> {
+
+    val classes = mutableListOf<String>()
+    var currentClass: KClass<*> = this::class
+
+    while (currentClass != Morphology::class) {
+        classes.add(0, currentClass.simpleName!!)
+        currentClass = (currentClass.java.superclass as Class<*>).kotlin
+    }
+
+    return classes.toList()
+  }
 }
