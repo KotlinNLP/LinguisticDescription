0ecec21a5e034a3573c596478d1679f74ba1fca4
KotlinNLP Authors <github@kotlinnlp.com>
2018-02-17 18:18:49 +0100

    Rename file

--- examples/SerializeDictionary.kt
+++ examples/SerializeDictionary.kt
@@ -11,7 +11,7 @@ import java.io.File
 import java.io.FileOutputStream
 
 /**
- * Load a [MorphologyDictionary] from a file in JSONL format.
+ * Load a [MorphologyDictionary] from a file in JSONL format and serialize it to another file.
  *
  * Command line arguments:
  *   1. The path of the input file from which to read the dictionary in JSONL format.
