a3740c043c05fd3dbd47713aaf150c5698e8c64b
KotlinNLP Authors <github@kotlinnlp.com>
2018-02-18 17:07:54 +0100

    Add ReadDictionary

---
+++ examples/ReadDictionary.kt
@@ -0,0 +1,44 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+import com.kotlinnlp.linguisticdescription.morphology.MorphologyDictionary
+import utils.Timer
+import java.io.File
+import java.io.FileInputStream
+
+/**
+ * Load a serialized [MorphologyDictionary] from a file and read values from it.
+ *
+ * Command line arguments:
+ *   1. The path of the input file from which to read the serialized dictionary.
+ */
+fun main(args: Array<String>) {
+
+  require(args.size == 1) { "Required 1 argument: <input_file>." }
+
+  val inputFile: String = args[0]
+  val timer = Timer()
+
+  println("Loading serialized dictionary from '$inputFile'...")
+  val m = MorphologyDictionary.load(FileInputStream(File(inputFile)))
+  println("Elapsed time: %s".format(timer.formatElapsedTime()))
+
+  println("\nNumber of elements in the dictionary: ${m.size}.")
+
+  var exit = false
+
+  while (!exit) {
+
+    print("\nSearch a form (empty to exit): ")
+    val searchVal = readLine()!!
+
+    if (searchVal.isEmpty())
+      exit = true
+    else
+      println("\n" + m[searchVal])
+  }
+}
