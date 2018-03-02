06d30ce8fd869e087242dff21bc4aa86c2e1d6d3
KotlinNLP Authors <github@kotlinnlp.com>
2018-03-02 23:01:29 +0100

    Add static method

--- src/main/kotlin/com/kotlinnlp/linguisticdescription/morphology/morphologies/Morphology.kt
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/morphology/morphologies/Morphology.kt
@@ -9,7 +9,9 @@ package com.kotlinnlp.linguisticdescription.morphology.morphologies
 
 import com.kotlinnlp.linguisticdescription.morphology.MorphologyType
 import com.kotlinnlp.linguisticdescription.morphology.properties.MorphologyProperty
+import com.kotlinnlp.linguisticdescription.morphology.properties.interfaces.*
 import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
@@ -19,6 +21,34 @@ import kotlin.reflect.full.primaryConstructor
  * @property lemma the lemma
  */
 abstract class Morphology(val lemma: String) {
+
+  companion object {
+
+    /**
+     * Get the list of properties required to build a certain morphology.
+     *
+     * @param type a morphology type
+     *
+     * @return a list of required properties for a morphology of the given type
+     */
+    fun getRequiredProperties(type: MorphologyType): List<String> {
+
+      val requiredProperties = mutableListOf<String>()
+      val morphologyClass: KClass<*> = morphologyClasses.getValue(type)
+
+      if (morphologyClass.isSubclassOf(Genderable::class)) requiredProperties.add("gender")
+      if (morphologyClass.isSubclassOf(Numerable::class)) requiredProperties.add("number")
+      if (morphologyClass.isSubclassOf(PersonDeclinable::class)) requiredProperties.add("person")
+      if (morphologyClass.isSubclassOf(Gradable::class)) requiredProperties.add("degree")
+      if (morphologyClass.isSubclassOf(CaseDeclinable::class)) requiredProperties.add("case")
+      if (morphologyClass.isSubclassOf(Conjugable::class)) {
+        requiredProperties.add("mood")
+        requiredProperties.add("tense")
+      }
+
+      return requiredProperties
+    }
+  }
 
   /**
    * The type associated to this morphology.
