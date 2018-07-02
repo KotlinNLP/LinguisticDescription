4474d57ba17d50ee3e8f3f443e0477158be59ec2
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-02 17:33:13 +0200

    Add RealSentence interface

---
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/RealSentence.kt
@@ -0,0 +1,22 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.linguisticdescription.sentence
+
+import com.kotlinnlp.linguisticdescription.sentence.token.Token
+import com.kotlinnlp.linguisticdescription.sentence.token.properties.Position
+
+/**
+ * A real sentence, with a position in the original text.
+ */
+interface RealSentence<TokenType: Token> : Sentence<TokenType> {
+
+  /**
+   * The position of this sentence in the original text.
+   */
+  val position: Position
+}
