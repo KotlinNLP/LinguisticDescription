cd407513ef8062f6a539a85bacac6cf8600f06a3
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-14 01:35:49 +0200

    Replace property

--- src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/token/LexicalToken.kt
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/token/LexicalToken.kt
@@ -1,6 +1,13 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
 package com.kotlinnlp.linguisticdescription.sentence.token
 
-import com.kotlinnlp.linguisticdescription.morphology.Morphology
+import com.kotlinnlp.linguisticdescription.morphology.LexicalInterpretation
 
 /**
  * The LexicalToken.
@@ -8,7 +15,7 @@ import com.kotlinnlp.linguisticdescription.morphology.Morphology
 interface LexicalToken : Token {
 
   /**
-   * The list of lexical interpretations (lemma and morphological properties)
+   * The list of lexical interpretations, sorted by score in a descending order.
    */
-  val lexicalForms: List<Morphology>
+  val lexicalInterpretations: List<LexicalInterpretation>
 }
