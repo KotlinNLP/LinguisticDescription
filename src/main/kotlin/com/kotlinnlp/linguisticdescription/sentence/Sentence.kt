332a40d631208ae92b891aa36e86c3cf5e481e6f
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-19 12:34:54 +0200

    Add buildText() method

--- src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/Sentence.kt
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/Sentence.kt
@@ -7,6 +7,7 @@
 
 package com.kotlinnlp.linguisticdescription.sentence
 
+import com.kotlinnlp.linguisticdescription.sentence.token.RealToken
 import com.kotlinnlp.linguisticdescription.sentence.token.Token
 
 /**
@@ -14,8 +15,42 @@ import com.kotlinnlp.linguisticdescription.sentence.token.Token
  */
 interface Sentence<TokenType: Token> {
 
+  companion object {
+
+    /**
+     * A separator used to construct the text of the sentence.
+     * One or more separators are put between tokens when there is a gap between the end of a token and the start of
+     * the following one.
+     */
+    private const val TOKENS_SEPARATOR = " "
+  }
+
   /**
    * The list of tokens of this sentence.
    */
   val tokens: List<TokenType>
+
+  /**
+   * @return the text of this sentence built from its [tokens]
+   */
+  fun buildText(): String {
+
+    val text = StringBuffer()
+    var tokenShouldStart = (this as? RealSentence)?.position?.start ?: 0
+
+    this.tokens.filter { it is RealToken }.forEach { it as RealToken
+
+      text.append(TOKENS_SEPARATOR.repeat(it.position.start - tokenShouldStart)) // multi-spaces start token padding
+      text.append(it.form)
+
+      tokenShouldStart = it.position.end + 1
+    }
+
+    if (this is RealSentence) {
+      // multi-spaces end sentence padding
+      text.append(TOKENS_SEPARATOR.repeat(this.position.end - tokenShouldStart + 1))
+    }
+
+    return text.toString()
+  }
 }
