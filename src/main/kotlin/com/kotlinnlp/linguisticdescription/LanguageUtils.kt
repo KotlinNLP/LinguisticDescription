111e2b8d6b90f4adf27225fc0a24ba6fcd6b4a80
KotlinNLP Authors <github@kotlinnlp.com>
2018-07-27 18:36:16 +0200

    Add LanguageUtils.kt

---
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/LanguageUtils.kt
@@ -0,0 +1,20 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.linguisticdescription
+
+/**
+ * A map of languages associated by ISO code.
+ */
+private val languagesByIso: Map<String, Language> = Language.values().associateBy { it.isoCode }
+
+/**
+ * @param isoCode the ISO 639-1 code of a language
+ *
+ * @return the language with the given code or null if no one corresponds
+ */
+fun getLanguageByIso(isoCode: String): Language? = languagesByIso[isoCode]
