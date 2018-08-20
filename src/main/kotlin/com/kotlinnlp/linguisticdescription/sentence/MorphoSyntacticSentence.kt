c37e81cd582243ccbe7501dc1012d6b998c01779
KotlinNLP Authors <github@kotlinnlp.com>
2018-08-20 12:50:51 +0200

    Make tokens mutable

--- src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/MorphoSyntacticSentence.kt
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/MorphoSyntacticSentence.kt
@@ -11,24 +11,72 @@ import com.beust.klaxon.JsonObject
 import com.beust.klaxon.json
 import com.kotlinnlp.linguisticdescription.sentence.properties.datetime.DateTime
 import com.kotlinnlp.linguisticdescription.sentence.properties.Entity
-import com.kotlinnlp.linguisticdescription.sentence.token.MorphoSyntacticToken
+import com.kotlinnlp.linguisticdescription.sentence.token.MutableMorphoSyntacticToken
 
 /**
  * A sentence with morphological and syntactic information.
  *
  * @property id the sentence id
  * @property confidence the confidence score
- * @property tokens the list of tokens of this sentence
  * @property dateTimes the list of date-times contained in this sentence (can be null)
  * @property entities the list of entities contained in this sentence (can be null)
  */
 data class MorphoSyntacticSentence(
   val id: Int,
   val confidence: Double,
-  override val tokens: List<MorphoSyntacticToken>,
   val dateTimes: List<DateTime>?,
   val entities: List<Entity>?
-) : SentenceIdentificable<MorphoSyntacticToken>() {
+) : SentenceIdentificable<MutableMorphoSyntacticToken>() {
+
+  companion object {
+
+    /**
+     * Build a [MorphoSyntacticSentence] with the given properties already assigned.
+     *
+     * @param id the id of the token, unique within its sentence
+     * @param confidence the confidence score
+     * @param tokens the list of tokens of this sentence
+     * @param dateTimes the list of date-times contained in this sentence (can be null)
+     * @param entities the list of entities contained in this sentence (can be null)
+     *
+     * @return a new sentence with the given properties
+     */
+    operator fun invoke(
+      id: Int,
+      confidence: Double,
+      tokens: List<MutableMorphoSyntacticToken>,
+      dateTimes: List<DateTime>?,
+      entities: List<Entity>?
+    ): MorphoSyntacticSentence {
+
+      val sentence =
+        MorphoSyntacticSentence(id = id, confidence = confidence, dateTimes = dateTimes, entities = entities)
+
+      sentence._tokens.addAll(tokens)
+
+      return sentence
+    }
+  }
+
+  /**
+   * The list of tokens of this sentence.
+   */
+  override val tokens: List<MutableMorphoSyntacticToken> get() = this._tokens
+
+  /**
+   * The mutable list of tokens of this sentence.
+   */
+  private val _tokens: MutableList<MutableMorphoSyntacticToken> = mutableListOf()
+
+  /**
+   * Add a new token at a given index.
+   *
+   * @param index the index of the [tokens] list at which to insert the token
+   * @param token the token to add
+   */
+  fun addToken(index: Int, token: MutableMorphoSyntacticToken) {
+    this._tokens.add(index, token)
+  }
 
   /**
    * Get a list of tokens with a given governor.
@@ -37,7 +85,7 @@ data class MorphoSyntacticSentence(
    *
    * @return a list of dependents tokens
    */
-  fun getDependents(governorId: Int): List<MorphoSyntacticToken> = this.tokens.filter {
+  fun getDependents(governorId: Int): List<MutableMorphoSyntacticToken> = this.tokens.filter {
     it.dependencyRelation.governor == governorId
   }
 
