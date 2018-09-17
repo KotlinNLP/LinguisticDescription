9ef50ab82be31fab1d5fd2470f481023a4067c23
KotlinNLP Authors <github@kotlinnlp.com>
2018-09-17 18:23:25 +0200

    Update pos to string

--- src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/token/MorphoSyntacticToken.kt
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/token/MorphoSyntacticToken.kt
@@ -32,7 +32,7 @@ interface MorphoSyntacticToken : ScoredMorphoToken, SyntacticToken {
       obj(
         "id" to self.id,
         "type" to self.type,
-        "pos" to self.pos?.labels?.joinToString("+"),
+        "pos" to self.pos?.toString(),
         "dependency" to self.syntacticRelation.toJSON(),
         "coReferences" to self.coReferences?.let { array(it.map { it.toJSON() }) },
         "semanticRelations" to self.semanticRelations?.let { array(it.map { it.toString() }) },
