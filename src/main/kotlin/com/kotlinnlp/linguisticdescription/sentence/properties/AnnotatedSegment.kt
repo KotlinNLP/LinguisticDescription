3e50585ae5d6ab80ad08f7f99fcf9c58e55366c4
KotlinNLP Authors <github@kotlinnlp.com>
2018-12-16 15:45:04 +0100

    Add AnnotatedSegment class

---
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/properties/AnnotatedSegment.kt
@@ -0,0 +1,21 @@
+/* Copyright 2017-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.linguisticdescription.sentence.properties
+
+/**
+ * An annotated segment of tokens.
+ *
+ * @property startToken the index of the first token of this segment, within the input tokens list
+ * @property endToken the index of the last token of this segment, within the input tokens list
+ * @property annotation the annotation
+ */
+data class AnnotatedSegment(
+  override val startToken: Int,
+  override val endToken: Int,
+  val annotation: String
+) : TokensRange
