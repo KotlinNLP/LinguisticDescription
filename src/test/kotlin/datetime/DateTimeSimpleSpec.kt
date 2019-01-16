582d9a0e1820b70c9d3a1783f89747f6d441bdb0
KotlinNLP Authors <github@kotlinnlp.com>
2019-01-16 19:36:45 +0100

    Add tests

--- src/test/kotlin/datetime/DateTimeSimpleSpec.kt
+++ src/test/kotlin/datetime/DateTimeSimpleSpec.kt
@@ -15,6 +15,7 @@ import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import java.time.LocalDateTime
 import kotlin.test.assertEquals
 
 /**
@@ -24,7 +25,7 @@ class DateTimeSimpleSpec : Spek({
 
   describe("a DateTimeSimple") {
 
-    context("12 Set 2018 10:35") {
+    context("12th September at 10:35") {
 
       val dateTime = DateTimeSimple(
         startToken = 0,
@@ -35,7 +36,7 @@ class DateTimeSimpleSpec : Spek({
           day = 12,
           weekDay = null,
           month = 9,
-          year = 2018,
+          year = null,
           yearAbbr = false,
           holiday = null),
         time = Time(
@@ -51,7 +52,16 @@ class DateTimeSimpleSpec : Spek({
       on("isoFormat()") {
 
         it("should return the expected string in ISO format") {
-          assertEquals("2018-09-12T10:35:00", dateTime.isoFormat())
+          assertEquals("0000-09-12T10:35:00", dateTime.isoFormat())
+        }
+      }
+
+      on("isoFormat() based on Friday 2018-04-13T09:15:10") {
+
+        val ref = LocalDateTime.of(2018, 4, 13, 9, 15, 10)
+
+        it("should return the expected string in ISO format") {
+          assertEquals("2018-09-12T10:35:00", dateTime.isoFormat(ref))
         }
       }
     }
