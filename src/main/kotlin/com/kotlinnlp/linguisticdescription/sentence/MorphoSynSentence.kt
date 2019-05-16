3c50b2a7a28707107a7acee1b38c311ee7e8d902
KotlinNLP Authors <github@kotlinnlp.com>
2019-05-16 13:32:40 +0200

    Make the MorphoSynSentence a RealSentence

--- src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/MorphoSynSentence.kt
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/MorphoSynSentence.kt
@@ -10,14 +10,20 @@ package com.kotlinnlp.linguisticdescription.sentence
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.json
 import com.kotlinnlp.linguisticdescription.sentence.token.MorphoSynToken
+import com.kotlinnlp.linguisticdescription.sentence.token.properties.Position
+import com.kotlinnlp.linguisticdescription.sentence.token.properties.Positionable
 
 /**
  * A sentence with morphological and syntactic information.
  *
  * @property id the sentence id
  * @property confidence the confidence score given by the system to the analysis of this sentence
+ * @property position the position of the sentence in the text
  */
-data class MorphoSynSentence(val id: Int, var confidence: Double = 0.0) : SentenceIdentificable<MorphoSynToken>() {
+data class MorphoSynSentence(
+  val id: Int,
+  var confidence: Double = 0.0
+) : RealSentence<MorphoSynToken>, SentenceIdentificable<MorphoSynToken>() {
 
   companion object {
 
@@ -32,7 +38,23 @@ data class MorphoSynSentence(val id: Int, var confidence: Double = 0.0) : Senten
      */
     operator fun invoke(id: Int, confidence: Double, tokens: List<MorphoSynToken>): MorphoSynSentence =
       MorphoSynSentence(id = id, confidence = confidence)
-        .apply { _tokens.addAll(tokens) }
+        .apply { tokens.forEach { this.addToken(it) } }
+
+    /**
+     * Build a [MorphoSynSentence] with a given list of tokens and a position.
+     *
+     * @param id the id of the token, unique within its sentence
+     * @param confidence the confidence score
+     * @param tokens the list of tokens of this sentence
+     *
+     * @return a new sentence with the given properties
+     */
+    operator fun invoke(id: Int,
+                        confidence: Double,
+                        tokens: List<MorphoSynToken>,
+                        position: Position): MorphoSynSentence =
+      MorphoSynSentence(id = id, confidence = confidence, tokens = tokens)
+        .apply { setPosition(position) }
   }
 
   /**
@@ -46,14 +68,24 @@ data class MorphoSynSentence(val id: Int, var confidence: Double = 0.0) : Senten
   private val _tokens: MutableList<MorphoSynToken> = mutableListOf()
 
   /**
+   * The position of the sentence in the text.
+   */
+  override val position: Position get() = this._position
+
+  /**
+   * The mutable position of the sentence.
+   */
+  private lateinit var _position: Position
+
+  /**
    * Add a new token at a given index.
    *
-   * @param index the index of the [tokens] list at which to insert the token
    * @param token the token to add
+   * @param index the index of the [tokens] list at which to insert the token (if null insert as last)
    */
-  fun addToken(index: Int, token: MorphoSynToken) {
+  fun addToken(token: MorphoSynToken, index: Int? = null) {
 
-    this._tokens.add(index, token)
+    this._tokens.add(index ?: this._tokens.size, token)
 
     this.reIndexTokens()
   }
@@ -70,6 +102,25 @@ data class MorphoSynSentence(val id: Int, var confidence: Double = 0.0) : Senten
       is MorphoSynToken.Single -> if (tk._syntacticRelation.governor == governorId) listOf(tk) else listOf()
       is MorphoSynToken.Composite -> tk.components.filter { it.syntacticRelation.governor == governorId }
     }
+  }
+
+  /**
+   * Set the position of this sentence.
+   */
+  fun setPosition(position: Position) {
+    this._position = position
+  }
+
+  /**
+   * Set the position of this sentence automatically, based on the position of its tokens.
+   */
+  fun autoSetPosition() {
+
+    this._position = Position(
+      index = if (this::_position.isInitialized) this._position.index else 0,
+      start = this._tokens.asSequence().mapNotNull { it as? Positionable }.firstOrNull()?.position?.start ?: 0,
+      end = this._tokens.asSequence().mapNotNull { it as? Positionable }.lastOrNull()?.position?.end ?: 0
+    )
   }
 
   /**
