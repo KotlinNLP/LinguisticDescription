90b03d408f974845fbe7b5084edd4e3bdb1db9ee
KotlinNLP Authors <github@kotlinnlp.com>
2020-03-05 17:00:07 +0100

    Fix documentation

--- src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/properties/datetime/Date.kt
+++ src/main/kotlin/com/kotlinnlp/linguisticdescription/sentence/properties/datetime/Date.kt
@@ -19,7 +19,7 @@ import java.time.LocalDateTime
  * @property endToken the index of the last token of this expression
  * @property day the number of the day in the range [1, 31] (can be null)
  * @property weekDay the number of the week day in the range [1, 7] (can be null)
- * @property month the number of the mont in the range [1, 12] (can be null)
+ * @property month the number of the month in the range [1, 12] (can be null)
  * @property year the number of the year in the range [0, 9999] (can be null)
  * @property yearAbbr whether the [year] is intended as abbreviated form (e.g. '98 stands for 1998)
  * @property holiday a holiday name (can be null)
